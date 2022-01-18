f89e5d1489e105631c864006ed9d3670bf13fcf1
Ridegras <franciscominutti.fm@gmail.com>
2022-01-18 19:56:25 -0300

    feat(Created PostsContainer component): client
    Just the inicialization of the component, i will countinue with this tomorrow.

---
+++ client/src/components/PostsContainer.tsx
@@ -0,0 +1,18 @@
+
+const PostsContainer = () => {
+
+    // Here we should work with the redux hooks and typescript..
+
+  return (
+    <>
+        {/* This component should recieve the posts array */}
+        {/*
+            posts.map(p => (
+                <PostCard  />
+            ))
+        */}
+    </>
+  );
+};
+
+export default PostsContainer;
