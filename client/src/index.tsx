139afaaef0aeb981fafcf5184f33c3951b332a9b
Franh01 <fran_lopez9@live.com>
2022-01-18 15:55:15 -0300

    feat(login-logout-register): finished ✨

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,18 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import store from './redux/store';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import store from "./redux/store";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Auth0Provider
+      domain="dev-37m4eh5q.us.auth0.com"
+      clientId="El9e66B2UpgeMnrRcsMKikg5r1ORlqvT"
+      redirectUri={window.location.origin}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Auth0Provider>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
