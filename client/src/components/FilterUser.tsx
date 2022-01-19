5113b373d82561330c626538b213cc83247e0ea8
Bryan De La Cruz <bryan.delacruza@gmail.com>
2022-01-19 09:45:05 -0500

    feat(FilterUser): modified ✨

--- client/src/components/FilterUser.tsx
+++ client/src/components/FilterUser.tsx
@@ -3,6 +3,22 @@ import React from "react";
 const FilterUser = () => {
   return (
     <div>
+      <h3>Filtros</h3>
+      <div>
+        <h4>Generales</h4>
+        <div>
+          <input
+            type="checkbox"
+            id="location"
+            name="location"
+            value="LOCATION"
+          />
+          <label htmlFor="location">Locación</label>
+          <br />
+          <input type="checkbox" id="score" name="score" value="SCORE" />
+          <label htmlFor="score">Calificación</label>
+        </div>
+      </div>
       <div>
         <h4>Modalidad</h4>
         <div>
@@ -19,6 +35,52 @@ const FilterUser = () => {
           <br />
           <input type="radio" id="hibrida" name="modalidad" value="HIBRIDA" />
           <label htmlFor="hibrida">Híbrida</label>
+        </div>
+      </div>
+      <div>
+        <h4>Tipo de contrado</h4>
+        <div>
+          <input
+            type="radio"
+            id="fullTime"
+            name="contractType"
+            value="FULLTIME"
+          />
+          <label htmlFor="fullTime">Jornada Completa</label>
+          <br />
+          <input
+            type="radio"
+            id="partTime"
+            name="contractType"
+            value="PARTTIME"
+          />
+          <label htmlFor="partTime">Medio Tiempo</label>
+          <br />
+          <input
+            type="radio"
+            id="temporary"
+            name="contractType"
+            value="TEMPORARY"
+          />
+          <label htmlFor="temporary">Temporal</label>
+          <br />
+          <input
+            type="radio"
+            id="perHour"
+            name="contractType"
+            value="PERHOUR"
+          />
+          <label htmlFor="perHour">por Hora</label>
+        </div>
+      </div>
+      <div>
+        <h4>Categorías</h4>
+        <div>
+          <input type="checkbox" id="cat1" name="cat1" value="CAT1" />
+          <label htmlFor="location">Categoría 1</label>
+          <br />
+          <input type="checkbox" id="cat2" name="cat2" value="CAT2" />
+          <label htmlFor="cat2">Categoría 2</label>
         </div>
       </div>
     </div>
