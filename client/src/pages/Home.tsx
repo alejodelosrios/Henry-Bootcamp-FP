a69aed6b744af0636e2efb8d3582bbe35103ab3c
Bryan De La Cruz <bryan.delacruza@gmail.com>
2022-01-21 10:12:13 -0500

    feature(home-page): update ✨

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,10 +1,21 @@
-import SearchBar from '../components/SearchBar'
+import SearchBar from "../components/SearchBar";
 // import { NavBar } from '../components/NavBar'
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Home = () => {
-    return (
-        <>
-            {/* {<NavBar/>} */}
+  return (
+    <>
+      {/* {<NavBar/>} */}
+      <Link to={"/"}>
+        {" "}
+        {/* Route to Post Creater */}
+        <button>Create New Post</button>
+      </Link>
+      <SearchBar /> {/* <-- Finish SB component!! */}
+      <span>{"<CompanyFilter/>"} Component</span>
+      <span>{"<PostsContainer/>"} Component</span>
+    </>
+  );
+};
 
 export default Home;
