e6e5af4f6cfbb03b35e7fba573c65eca4103f747
Franh01 <fran_lopez9@live.com>
2022-01-21 19:50:42 -0300

    fix(navBar): bug fixed

--- client/src/pages/WelcomePage/WelcomePage.tsx
+++ client/src/pages/WelcomePage/WelcomePage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import SearchBar from "../../components/SearchBar";
+import SearchBar from "../../components/SearchBar";
 // import { NavBar } from "../../components/NavBar";
 import {
   Section, Card, Image, Title, 
@@ -27,7 +27,7 @@ const WelcomePage: React.FC = () => {
           </Paragraph>
         </Card>
 
-        {/* <SearchBar /> */}
+        <SearchBar />
       </Section>
 
       <CategorySec>
