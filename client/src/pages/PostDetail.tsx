806b51266490074468a5145f13e1fe3de3486530
Franh01 <fran_lopez9@live.com>
2022-01-21 15:09:47 -0300

    fix(postDetail): navBar mal renderizada🐛

--- client/src/pages/PostDetail.tsx
+++ client/src/pages/PostDetail.tsx
@@ -1,10 +1,8 @@
 import React from "react";
-import { NavBar } from "../components/NavBar";
 import PostDetail from "../components/PostDetail";
 export const PostDetailPage = () => {
   return (
     <div>
-      <NavBar />
       <PostDetail />
     </div>
   );
