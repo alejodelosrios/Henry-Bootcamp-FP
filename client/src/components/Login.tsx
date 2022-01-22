c5b6707122319eb62b0fdf6c4252956d4125ad07
Bryan De La Cruz <bryan.delacruza@gmail.com>
2022-01-21 20:10:14 -0500

    featl(login-navbar-searchbar): updated ✨

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,18 +1,15 @@
-import React, { FC } from 'react';
-import styled from 'styled-components';
+import React, { FC } from "react";
+import styled, { css } from "styled-components";
 
 interface Props {
-    contenido: string
-    estilos: string
+  contenido: string;
+  estilos: string;
 }
-
+const Button = styled.button``;
 export const Login: FC<Props> = ({ contenido, estilos }) => {
-    const google = () => {
-        window.open("http://localhost:3001/auth/google", "_self");
-    };
+  const google = () => {
+    window.open("http://localhost:3001/auth/google", "_self");
+  };
 
-    const Button = styled.button`
-    ${estilos}
-`
-    return <Button onClick={google}>{contenido}</Button>;
+  return <Button onClick={google}>{contenido}</Button>;
 };
--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,40 +1,40 @@
-import React, { FC } from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-import { Login } from './Login';
-import { Logout } from './Logout';
-import logo from '../assets/logo.svg'
+import React, { FC } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { Login } from "./Login";
+import { Logout } from "./Logout";
+import logo from "../assets/logo.svg";
 
 const A = styled.a`
-    text-decoration: none;
-    font-size: 20px;
-    color: #757577;
-    font-weight: bolder;
-    margin-right: 1rem;   
-    font-family: Open sans/bold;
-    padding: 0px 1rem;
-`
+  text-decoration: none;
+  font-size: 20px;
+  color: #757577;
+  font-weight: bolder;
+  margin-right: 1rem;
+  font-family: Open sans/bold;
+  padding: 0px 1rem;
+`;
 const Name = styled.a`
-    text-decoration: none;
-    font-size: 16px;
-    color: black;
-    font-weight: bolder;    
-    cursor: pointer;
-    margin-right: 1rem;
-`
+  text-decoration: none;
+  font-size: 16px;
+  color: black;
+  font-weight: bolder;
+  cursor: pointer;
+  margin-right: 1rem;
+`;
 
 const MainFlexDiv = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    align-items: center;
-    padding: 20px;
-`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  padding: 20px;
+`;
 const FlexDiv = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
 const estilos = `
     background: #c879ff;
     border: none;
@@ -44,53 +44,56 @@ const estilos = `
     font-size: 20px;
     color: white;
     font-family: Open sans/Regular;
-`
+`;
 
 interface Props {
-    userLogged: any
+  userLogged: any;
 }
 
 export const NavBar: FC<Props> = ({ userLogged }) => {
-    console.log(userLogged)
-    if (userLogged) {
-        return (
-            <div>
-                <MainFlexDiv className='flex-container'>
-                    <FlexDiv className='imagen-logo'>
-                        <Link to='/' style={{ textDecoration: 'none' }}><img src={logo} alt="logo" /></Link>
-                    </FlexDiv>
-                    <div className='multi-options'>
-                        <A href="/">{'Inicio'}</A>
-                        <A href="">{'Postulantes'}</A>
-                        <A href="">{'Empresas'}</A>
-                        <A href="">{'Nosotros'}</A>
-                    </div>
-                    <div>
-                        <Name href='/profile'>{userLogged?.displayName}</Name>
-                        <Logout contenido='Logout' estilos={estilos}/>
-                    </div>
-                </MainFlexDiv>
-            </div>
-        )
-    } else {
-        return (
-            <div>
-                <MainFlexDiv className='flex-container'>
-                    <FlexDiv className='imagen-logo'>
-                        <Link to='/' style={{ textDecoration: 'none' }}><img src={logo} alt="logo" /></Link>
-                    </FlexDiv>
-                    <div className='multi-options'>
-                        <A href="">{'Inicio'}</A>
-                        <A href="">{'Postulantes'}</A>
-                        <A href="">{'Empresas'}</A>
-                        <A href="">{'Nosotros'}</A>
-                    </div>
-                    <div className='login'>
-                        <Login contenido='Ingresar' estilos={estilos} />
-                    </div>
-                </MainFlexDiv>
-            </div>
-        )
-    }
-    
-}
+  // console.log(userLogged)
+  if (userLogged) {
+    return (
+      <div>
+        <MainFlexDiv className="flex-container">
+          <FlexDiv className="imagen-logo">
+            <Link to="/" style={{ textDecoration: "none" }}>
+              <img src={logo} alt="logo" />
+            </Link>
+          </FlexDiv>
+          <div className="multi-options">
+            <A href="/">{"Inicio"}</A>
+            <A href="">{"Postulantes"}</A>
+            <A href="">{"Empresas"}</A>
+            <A href="">{"Nosotros"}</A>
+          </div>
+          <div>
+            <Name href="/profile">{userLogged?.displayName}</Name>
+            <Logout contenido="Logout" estilos={estilos} />
+          </div>
+        </MainFlexDiv>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <MainFlexDiv className="flex-container">
+          <FlexDiv className="imagen-logo">
+            <Link to="/" style={{ textDecoration: "none" }}>
+              <img src={logo} alt="logo" />
+            </Link>
+          </FlexDiv>
+          <div className="multi-options">
+            <A href="">{"Inicio"}</A>
+            <A href="">{"Postulantes"}</A>
+            <A href="">{"Empresas"}</A>
+            <A href="">{"Nosotros"}</A>
+          </div>
+          <div className="login">
+            <Login contenido="Ingresar" estilos={estilos} />
+          </div>
+        </MainFlexDiv>
+      </div>
+    );
+  }
+};
