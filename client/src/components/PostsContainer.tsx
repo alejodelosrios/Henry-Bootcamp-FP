44af38687420167e6bbce72560a733dfb209d94b
Bryan De La Cruz <bryan.delacruza@gmail.com>
2022-01-21 20:06:34 -0500

    feat(posts-container): updated ✨

--- client/src/components/PostsContainer.tsx
+++ client/src/components/PostsContainer.tsx
@@ -1,99 +1,17 @@
 import { useDispatch, useSelector } from "react-redux";
+import rootReducers from "../redux/reducers/rootReducer";
 import Post from "./Post";
 
 const PostsContainer = () => {
-  // Here we should work with the redux hooks and typescript..
-  const dispatch = useDispatch();
-  const posts = [
-    {
-      id: 1,
-      title: "full stack developer",
-      experience: {
-        min: 3,
-        max: 5,
-      },
-      description:
-        "Descripción del puesto de lo cual solo se extraerá un número limitado de caracteres para que encaje en el Post",
-      tags: ["javascript", "react", "express"],
-      modality: "remote",
-      contractType: "full time",
-      location: { country: "Uruguay", state: "montevideo" },
-      startDate: "20/01/2022",
-      endDate: "30/01/2022",
-      category: "Tecnología",
-      salary: {
-        min: 1000,
-        max: 2000,
-      },
-    },
-    {
-      id: 2,
-      title: "Pintor",
-      experience: {
-        min: 3,
-        max: 5,
-      },
-      description:
-        "Descripción del puesto de lo cual solo se extraerá un número limitado de caracteres para que encaje en el Post",
-      tags: ["decoración", "pintura", "acabados"],
-      modality: "presencial",
-      contractType: "part time",
-      location: { country: "Mexico", state: "Guadalajara" },
-      startDate: "23/01/2022",
-      endDate: "02/02/2022",
-      category: "Contrucción",
-      salary: {
-        min: 1000,
-        max: 2000,
-      },
-    },
-    {
-      id: 3,
-      title: "full stack developer 2",
-      experience: {
-        min: 3,
-        max: 5,
-      },
-      description:
-        "Descripción del puesto de lo cual solo se extraerá un número limitado de caracteres para que encaje en el Post",
-      tags: ["javascript", "react", "express"],
-      modality: "remote",
-      contractType: "full time",
-      location: { country: "Uruguay", state: "montevideo" },
-      startDate: "20/01/2022",
-      endDate: "30/01/2022",
-      category: "Tecnología",
-      salary: {
-        min: 1000,
-        max: 2000,
-      },
-    },
-    {
-      id: 4,
-      title: "Pintor 2",
-      experience: {
-        min: 3,
-        max: 5,
-      },
-      description:
-        "Descripción del puesto de lo cual solo se extraerá un número limitado de caracteres para que encaje en el Post",
-      tags: ["decoración", "pintura", "acabados"],
-      modality: "presencial",
-      contractType: "part time",
-      location: { country: "Mexico", state: "Guadalajara" },
-      startDate: "23/01/2022",
-      endDate: "02/02/2022",
-      category: "Contrucción",
-      salary: {
-        min: 1000,
-        max: 2000,
-      },
-    },
-  ];
+  const currentPosts = useSelector(
+    (state: any) => state.postsReducer.currentPosts
+  );
+
+  // console.log(currentPosts);
 
   return (
     <div>
-      {posts.map((post) => (
+      {currentPosts.map((post: any) => (
         <Post
           key={post.id}
           id={post.id}
