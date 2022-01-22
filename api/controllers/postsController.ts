cd482e74da9fcaf03fcb78e2d13d0a201c96940a
Julian <j.m.decurnex@gmail.com>
2022-01-22 17:23:16 -0300

    erased unnecessary example object

--- api/controllers/postsController.ts
+++ api/controllers/postsController.ts
@@ -147,22 +147,6 @@ module.exports = {
     }
 }
 
-
-const ejemplo = {
-    inputName:"", // -> full stack* (genérico)
-    modality: "", // -> presencial/remote/hybrid (solo habrá estas 3 opciones)
-    contractType: "", // -> fullTime/partTime/temporary/perHour (solo habrá estas 4 opciones)
-    location: [
-        {
-            country:"", // Perú/Argentina (genérico)
-            state:"",  // Cuzco/Caba (genérico)
-        }
-    ],
-    score: "", // -> "1"/"2"/"3"/"4"/"5" (solo habrá estas 5 opciones)
-    categories: "", // -> Technology/Health/Finance/Education/Humanities/Art (rubros genéricos, inicialmente hemos puesto 6, distinto a las Tags)
-    orderBy: "", // -> scoreHightToLow/ (por el momento solo se ha pensado 1 ordenamiento)
-}
-
 const ejemploBRYAN = {
     inputName:"",
     categories: ["Agriculture, Food, and Natural Resources", "Business and Finance"],
