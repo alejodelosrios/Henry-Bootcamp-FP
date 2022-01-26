7f21c4e50084e39488f9f2627798227e2a883d5d
María Celeste Medina <mariacelestemedinamaiuri@gmail.com>
2022-01-26 17:25:57 -0300

    feat(news): ✨ created routes and tested them

--- api/routes/index.ts
+++ api/routes/index.ts
@@ -7,7 +7,7 @@ const userRouter = require("./user");
 const educationRouter = require("./education");
 const languageRouter = require("./language");
 const experienceRouter = require("./experience");
-
+const newsRouter = require("./news");
 const mainRouter = Router();
 
 mainRouter.use("/language", languageRouter);
@@ -17,5 +17,6 @@ mainRouter.use("/posts", postsRouter);
 mainRouter.use("/company", companyRouter);
 mainRouter.use("/education", educationRouter);
 mainRouter.use("/experience", experienceRouter);
+mainRouter.use("/news", newsRouter);
 
 module.exports = mainRouter;
