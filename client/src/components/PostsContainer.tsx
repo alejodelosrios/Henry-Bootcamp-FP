6a7bae3aeb13c71f9ebaeb3fde77ef907d766bcc
Ridegras <franciscominutti.fm@gmail.com>
2022-01-26 15:40:48 -0300

    feat(Paginated Posts): ✨ paginated logic added

--- client/src/components/PostsContainer.tsx
+++ client/src/components/PostsContainer.tsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
+import Paginated from "./Paginated/Paginated";
 import Post from "./Post";
 
 const Container = styled.div`
@@ -16,20 +18,40 @@ const PostsContainer = () => {
     const currentPosts = useSelector(
         (state: any) => state.postsReducer.currentPosts
     );
+
+    //PAGINADO ////////////////////////////////////////////
+    const [currPage, setCurrPage] = useState(1);    
+    const PostsPerPage = 4;
+    const ixLastPost = currPage * PostsPerPage;
+    const ixFirstPost = ixLastPost - PostsPerPage;
+    const currPost = currentPosts.slice(ixFirstPost, ixLastPost);
+
+    const paginado = (pagNum: number)=>{
+        setCurrPage(pagNum);
+    }
+
     return (
-        <Container>
-            {currentPosts.map((post: any) => (
-                <Post
-                    key={post.postId}
-                    id={post.postId}
-                    title={post.title}
-                    location={post.location}
-                    modality={post.modality}
-                    salary={post.salary}
-                    startDate={post.startDate}
-                />
-            ))}
-        </Container>
+        <>
+            <Container>
+                {currPost.map((post: any) => (
+                    <Post
+                        key={post.postId}
+                        id={post.postId}
+                        title={post.title}
+                        location={post.location}
+                        modality={post.modality}
+                        salary={post.salary}
+                        startDate={post.startDate}
+                    />
+                ))}
+            </Container>
+            
+            <Paginated 
+                PostsPerPage={PostsPerPage}
+                AllPostsLength={currentPosts.length}
+                paginado={paginado}
+            />
+        </>
     );
 };
 
