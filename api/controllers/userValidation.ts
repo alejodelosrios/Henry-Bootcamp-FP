167ceb1bcd5377e13c353dbd2c58557ad1520cfe
Alejandro Ramirez <ramalejandro@gmail.com>
2022-01-27 12:44:18 -0600

    feat(Backend V2 integration): Work under construction 🚧
    - Google and own login fixed
    - Google and own register fixed
    - Choose role modal fixed

--- api/controllers/userValidation.ts
+++ api/controllers/userValidation.ts
@@ -1,18 +1,32 @@
 import { prisma } from "../prisma/database";
 
 module.exports = {
-    getUserIfExists: async function (email:string){
-        const users = await prisma.user.findMany()
-        users.forEach(user => {
-            if(user.email === email) return user
-            else return "No se encontro el usuario"
-        })
+    getUserIfExists: async function (email: string) {
+        console.log(email);
+        const user = await prisma.user.findMany({
+            where: {
+                email: email,
+            },
+            include: {
+                applicant: true,
+                company: true,
+            },
+        });
+        if (user[0]) {
+            return user[0];
+        }
+        return "No se encontro el usuario";
+
+        //users.forEach(user => {
+        //if(user.email === email) return user
+        //else return "No se encontro el usuario"
+        //})
     },
-    checkIfEmailAvailable: async function (email:string){
-        const users = await prisma.user.findMany()
-        users.forEach(user => {
-            if(user.email === email) return false
-            else return true
-        })
-    }
-}
+    checkIfEmailAvailable: async function (email: string) {
+        const users = await prisma.user.findMany();
+        users.forEach((user) => {
+            if (user.email === email) return false;
+            else return true;
+        });
+    },
+};
