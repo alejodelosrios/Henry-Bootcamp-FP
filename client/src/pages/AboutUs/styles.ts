c6a4b2873c99118492243deae2e33e73629ae6cf
Federico <federicofern@live.com.ar>
2022-01-27 03:09:12 -0300

    Feature (AboutUs): ✨ Components AboutUs - CardCont

--- client/src/pages/AboutUs/styles.ts
+++ client/src/pages/AboutUs/styles.ts
@@ -6,67 +6,147 @@ export const AboutCont = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  
 `;
 
-export const Header = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    margin-top: 4%;
+export const Title = styled.h1`
+    color: #FFFFFF;
+    font-size: 6vw;
+    margin-top: 6vw;
+    `
+export const TL = styled.span`
+    color: #EF5DA8;
+    font-size: 6vw;
+    margin-top: 6vw;
+    `
+
+export const SubTitle = styled.h3`
+    color: #EF5DA8;
+    font-size: 2vw;
+    margin-bottom: 2vw;
+    margin-top:3vw;
+`
+export const AsideTitle = styled.h3`
+    color: #EF5DA8;
+    font-size: 2vw;
+    margin-bottom: 2vw;
+    margin-top:3vw;
+`
+export const MiniTitle = styled.h3`
+    color: ${props=> props.theme.colors.typography.dark};
+    font-size: 1vw;
+    text-align: right;
 `
 
-export const Logo = styled.img`
-    width: 90%;
+export const Div = styled.div`
+    width: 50%;
+`
+
+export const Header = styled.div`
+    width: 100vw;
+    height: 23vw;
+    display: flex;
+    justify-content: center;
+    overflow: hidden;
+    align-items: center;
+    z-index: -10;
+    position: absolute;
+    top: 0;
+`
+
+export const Img = styled.img`
+    width: 100vw;
+    height: auto;
+`
+export const ImgTwo = styled.img`
+    width: 80%;
+    height: auto;
 `
 
 export const Section = styled.div`
     display: flex;
     align-items: center;
-    justify-content: space-around;
+    justify-content: space-evenly;
     width: 80%;
-    margin: 10% 0;
+    margin: 8% 0;
+    margin-bottom: 3%;
 `
 
 export const Description = styled.div`
-    width: 50%;
     text-align: left;
     color: ${props=> props.theme.colors.typography.dark};
     font-family: ${p=> p.theme.colors.typography.poppins};
+    font-size: 1vw;
+    position: relative;
+`
 
+export const SpanPink = styled.span`
+    color: #EF5DA8;
+    font-weight: bold;
+`
+
+export const Span = styled.span`
+position: absolute;
+font-size: 300px;
+line-height: 1;
+z-index:-1;
+color: #FFFFFF;
+top: -30px;
+left: -20px;
 `
 
 export const Aside = styled.div`
-    width: 20%;
+    width: 30%;
+    padding: 45px;
     display: flex;
     flex-direction: column;
+    justify-content: space-evenly;
     align-items: flex-end;
+    background-color:rgba(200, 121, 255, 0.1);
+    border-radius: 15px;
+    z-index: -10;
 `
 
-export const Graphic = styled.img`
-    width: 90%;
-    margin: 10% 0;
+export const DivBot = styled.div`
+display: flex;
+flex-wrap: wrap;
+justify-content: space-evenly;
+width: 80%;
+margin: 5% 0;
 `
 
-export const Info = styled.div`
-    text-align: right;
-    color: ${props=> props.theme.colors.typography.dark};
-    font-family: ${p=> p.theme.colors.typography.poppins};
+export const Card = styled.div`
+    display:flex;
+    background-color:rgba(200, 121, 255, 0.1);
+    border-radius: 15px;
+    justify-content: space-evenly;
+    padding: 20px;
+    margin: 10px;
+    width: 400px;
+    height: 200px;
+    align-items: center;
+`
+export const TeamImg = styled.img`
+    width: 100px;
+    height: 100px;
+    margin-right: 30px;
+`
+
+export const CardTitle = styled.h2`
+    color: #EF5DA8;
+    margin: 5% 0;
+`
+export const CardSubTitle = styled.h3`
+    color: #757577;
+    margin: 2% 0;
+`
+export const CardMiniTitle = styled.h4`
+    color: #757577;
+    margin: 1% 0;
 `
 
 export const Foot = styled.div`
-    width: 100%;
-    height: 50px;
-    background-color: ${p => p.theme.colors.backgrounds.cards};
-    display: flex;
-    justify-content: space-evenly;
-    align-items: center;
-    margin-bottom: 5%;
-`
-
-export const Us = styled.img`
-    background-color: white;
-    border-radius: 50%;
-    border: solid 3px ${p=> p.theme.colors.backgrounds.cards};
-    width: 80px;
-    height: 80px;
+    background-color: #FED2FF;
+    width: 100vw;
+    height: 30vw;
 `
