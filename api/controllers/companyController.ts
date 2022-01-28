eaa350be2f63eda93b2c099bb19fba8b9ca5c393
María Celeste Medina <mariacelestemedinamaiuri@gmail.com>
2022-01-28 14:55:04 -0300

    feat (company): update status

--- api/controllers/companyController.ts
+++ api/controllers/companyController.ts
@@ -4,7 +4,7 @@ import { Request, Response } from "express";
 module.exports = {
   create: async (req: Request, res: Response) => {
     try {
-      const { userId } = req.params
+      const { userId } = req.params;
       const {
         name,
         legalName,
@@ -16,15 +16,42 @@ module.exports = {
         vision,
       } = req.body;
 
-      if (!userId) return res.send("Debes incluir un campo 'userId' con el id del usuario al cual esta asociado esta compañia por params");
-      if (!name) return res.send("Debes incluir un campo 'name', puede contener una string vacía");
-      if (!legalName) return res.send("Debes incluir un campo 'legalName', puede contener una string vacía");
-      if (!stin) return res.send("Debes incluir un campo 'stin' (el 'cuil' en argentina), puede contener una string vacía");
-      if (!accountManagers) return res.send("Debes incluir un campo 'accountManagers', contiene un arreglo de strings, puede estar vacío");
-      if (!image) return res.send("Debes incluir un campo 'image', puede contener una string vacía");
-      if (!companyValues) return res.send("Debes incluir un campo 'companyValues', puede contener una string vacía");
-      if (!mission) return res.send("Debes incluir un campo 'mission', puede contener una string vacía");
-      if (!vision) return res.send("Debes incluir un campo 'vision', puede contener una string vacía");
+      if (!userId)
+        return res.send(
+          "Debes incluir un campo 'userId' con el id del usuario al cual esta asociado esta compañia por params"
+        );
+      if (!name)
+        return res.send(
+          "Debes incluir un campo 'name', puede contener una string vacía"
+        );
+      if (!legalName)
+        return res.send(
+          "Debes incluir un campo 'legalName', puede contener una string vacía"
+        );
+      if (!stin)
+        return res.send(
+          "Debes incluir un campo 'stin' (el 'cuil' en argentina), puede contener una string vacía"
+        );
+      if (!accountManagers)
+        return res.send(
+          "Debes incluir un campo 'accountManagers', contiene un arreglo de strings, puede estar vacío"
+        );
+      if (!image)
+        return res.send(
+          "Debes incluir un campo 'image', puede contener una string vacía"
+        );
+      if (!companyValues)
+        return res.send(
+          "Debes incluir un campo 'companyValues', puede contener una string vacía"
+        );
+      if (!mission)
+        return res.send(
+          "Debes incluir un campo 'mission', puede contener una string vacía"
+        );
+      if (!vision)
+        return res.send(
+          "Debes incluir un campo 'vision', puede contener una string vacía"
+        );
 
       const newCompany = await prisma.company.create({
         data: {
@@ -52,6 +79,48 @@ module.exports = {
       res.send(error);
     }
   },
+  // /company/application
+  updateStatus: async (req: Request, res: Response) => {
+    try {
+      const { applicantId, postId, newStatus } = req.body;
+      const updatedStatus = await prisma.applicantPool.updateMany({
+        where: {
+          applicantId,
+          postId,
+        },
+        data: {
+          status: newStatus,
+        },
+      });
+      const post = await prisma.post.findFirst({
+        where: {
+          id: Number(postId),
+        },
+      });
+
+      if (post && post.companyId) {
+        const companies = await prisma.company.findMany({
+          where: {
+            id: post.companyId,
+          },
+          include: {
+            posts: {
+              include: {
+                applicants: {
+                  include: {
+                    applicant: true,
+                  },
+                },
+              },
+            },
+          },
+        });
+        res.json(companies);
+      }
+    } catch (error) {
+      res.send(error);
+    }
+  },
   companyById: async (req: Request, res: Response) => {
     try {
       const { companyId } = req.params;
@@ -64,8 +133,8 @@ module.exports = {
           notifications: true,
           reviews: true,
           posts: true,
-          followers: true
-        }
+          followers: true,
+        },
       });
       res.json(company);
     } catch (error) {
