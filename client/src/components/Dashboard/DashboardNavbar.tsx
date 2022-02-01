1cab16eca820d60d2bf41613e801a0baa71bf752
Alejandro Ramirez <ramalejandro@gmail.com>
2022-02-01 11:02:35 -0600

    feat(DashboardNavbar): Action buttons added ✨

--- client/src/components/Dashboard/DashboardNavbar.tsx
+++ client/src/components/Dashboard/DashboardNavbar.tsx
@@ -1,16 +1,44 @@
-import { FC } from "react";
+import {FC} from "react";
+import {Link} from "react-router-dom";
 import styled from "styled-components";
+import {useSelector} from "react-redux";
+import Notifications from "../Notifications";
+import ProfModalButton from "../ProfModalButton";
+import Favorites from "../Favorites";
 
 const Container = styled.div`
   width: 100%;
   height: 60px;
-  background: gray;
+  display:flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 1rem 1.5rem;
+  border-bottom: 0.5px solid gray;
+`;
+
+const ButtonsContainer = styled.div`
+  z-index: 1000;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap:0.3rem;
 `;
 
 const DashboardNavbar: FC = () => {
+
+  const user = useSelector((state: any) => state.userReducer.applicant);
+  const role = useSelector((state: any) => state.userReducer.role);
   return (
     <Container>
-      <p>DashboardNavbar</p>
+      <ButtonsContainer>
+        <Notifications role={role} />
+        {role === "applicant" && (
+          <Favorites role={role} />
+        )}
+
+        <ProfModalButton user={user} />
+      </ButtonsContainer>
     </Container>
   );
 };
