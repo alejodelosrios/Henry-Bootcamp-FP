10b633e12191fc158076955a5f2984c942f52489
Alejandro Ramirez <ramalejandro@gmail.com>
2022-01-31 20:07:15 -0600

    feat(Profile): Component & styles adjusted to use Dashboard layout ✨

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,23 +1,23 @@
-import React, { FC } from 'react';
+import React, {FC} from 'react';
 
-import { UserProfile } from '../components/UserProfile/Profile';
-import { CompanyProfile } from '../components/CompanyProfile/CompanyProfile';
-import { useSelector } from 'react-redux';
+import {UserProfile} from '../components/UserProfile/Profile';
+import {CompanyProfile} from '../components/CompanyProfile/CompanyProfile';
+import {useSelector} from 'react-redux';
+import Dashboard from './Dashboard/Dashboard';
 //* navbar, info de contacto(tarjeta con info del usuario), experiencia, educacion, idiomas, similar a info de contacto, todo editable
 interface Props {
     user: any
 }
-export const Profile: FC<Props> = ({ user }) => {
+export const Profile: FC<Props> = ({user}) => {
     const role = useSelector((state: any) => state.userReducer.role)
     return (
-        <div>
-            <UserProfile/>
+        <Dashboard>
+            <UserProfile />
             {/* {
                 role === 'applicant' ? <UserProfile/>
                     :
                     <CompanyProfile/>
             } */}
-            
-        </div>
+        </Dashboard>
     )
 }
