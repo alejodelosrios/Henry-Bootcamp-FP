9661a2c6859ec8e1de0ce07b17711ae186de2aa6
Julian <j.m.decurnex@gmail.com>
2022-02-02 19:07:39 -0300

    feat(posts): :sparkles: added tweaked validateToken middleware

--- api/routes/middlewares/validateToken.ts
+++ api/routes/middlewares/validateToken.ts
@@ -2,8 +2,7 @@ import { Request, Response } from "express";
 
 module.exports = function validateToken () {
   return (req: Request, res: Response, next: Function) => {
-    const bearerHeader = req && req.headers.token
-    if(typeof bearerHeader !== 'undefined'){
+    if(req.headers.token){
       next()
     } else {
       res.status(403).send("Token invalida")
