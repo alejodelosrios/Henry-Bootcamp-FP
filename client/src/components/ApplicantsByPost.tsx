29dd105caa338a76c0a51e076c178621f2371e49
Ridegras <franciscominutti.fm@gmail.com>
2022-02-03 19:57:28 -0300

    feat(Applicants Table)

--- client/src/components/ApplicantsByPost.tsx
+++ client/src/components/ApplicantsByPost.tsx
@@ -1,6 +1,8 @@
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
 import ApplicantCard from "./ApplicantCard";
+import DataTable from "./DataTable";
 
 const Container = styled.div`
     width: 90%;
@@ -11,23 +13,68 @@ const Container = styled.div`
     align-items: center;
 `;
 
+const Filters = styled.div`
+    margin: 2% 0;
+    align-self: flex-start;
+     input{
+        height: 50px;
+        width: 350px;
+        background: none;
+        border: none;
+        border-radius: 20px;
+        padding-left: 4%;
+        box-shadow: -3px -3px 5px 3px rgb(255 255 255), 
+            2px 2px 5px 2px rgb(190 190 190);
+
+        &:focus{
+            color: ${p => p.theme.colors.typography.dark};
+            outline: none;
+            box-shadow: inset -3px -3px 5px 3px rgb(255 255 255), 
+                inset 2px 2px 5px 2px rgb(190 190 190);
+        }
+     }
+`;
+
 interface Props {
  applicants: any,
- postId: number
+ postId: number,
+ favorites: object[]
 }
 
-const ApplicantByPost: FC<Props> = ({applicants, postId}) => {
+const ApplicantByPost: FC<Props> = ({applicants, postId, favorites}) => {
+    const [q, setQ] = useState('');
+    
+    const search = (rows :any)=>{
+        const columns = rows[0] && Object.keys(rows[0])
+
+        return rows.filter(
+            columns.some((column:any) => 
+                rows[column].toString().toLowerCase().indexOf(q.toLowerCase()) > -1)
+        )
+    }
+
     return (
         <>
             <Container>
-                {applicants.map((ap: any) => (
+                <Filters>
+                    <input 
+                        type="text" 
+                        placeholder="Search"
+                        value={q} 
+                        onChange={e => setQ(e.target.value)}
+                    />
+                </Filters>
+                <DataTable data={applicants} postId={postId} favorites={favorites}/>
+
+
+                {/* {applicants.map((ap: any) => (
                     <ApplicantCard 
                         key={ap.id}
                         applicant = {ap.applicant}
                         applicantId={ap.applicantId}
                         postId={postId}
                     /> 
-                ))}
+                ))} */}
             </Container>
         </>
     );
---
+++ client/src/components/Td.tsx
@@ -0,0 +1,63 @@
+import { FC, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { setFavApplicant } from '../redux/actions/companyActionCreators';
+import { checkExistance } from '../services/checkExistance';
+
+interface Props {
+    column: any,
+    row: any,
+    postId:number,
+    favorites: object[]
+}
+
+const FavsApp = styled.div`
+    font-size: 30px;
+    cursor: pointer;
+    color: ${p => p.theme.colors.details.secondary2};
+`
+
+const Td: FC <Props> = ({column, row, postId}) => {
+    // const dispatch = useDispatch();
+
+    // const [isFav, setIsFav] = useState(checkExistance(favorites, row.applicantId));
+
+    // const handleFav = ()=>{
+    //     dispatch(setFavApplicant(row.applicantId, postId));
+    //     setIsFav(!isFav);
+    // }
+
+    const dispatch = useDispatch();
+
+    const {favorites} = useSelector(
+        (state:any)=> state.postsReducer.postById
+    )
+
+    // checkExistance(favorites, applicantId)
+    const [isFav, setIsFav] = useState(checkExistance(favorites, row.applicantId));
+    
+
+    const handleFav = ()=>{
+        dispatch(setFavApplicant(row.applicantId, postId));
+        setIsFav(!isFav);
+    }
+
+  return (
+    <td>{
+        column === 'image'
+            ? <Link to={`/applicant/${row.applicantId}`}>
+                <img src={row.applicant[column]} alt="" />
+            </Link>
+            : column === 'skillTags'
+                ? row.applicant[column].map((sk:any)=> <p>{sk}</p>)
+                : column === 'favorites' 
+                    ? <FavsApp onClick={handleFav}>
+                        {isFav ? '★' : '☆' }
+                    </FavsApp>
+                    : row.applicant[column] 
+    }</td>
+  );
+};
+
+export default Td;
