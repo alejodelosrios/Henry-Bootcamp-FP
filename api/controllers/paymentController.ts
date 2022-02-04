e310263596c2f343a1bb83368df0fa77104ce8cb
María Celeste Medina <mariacelestemedinamaiuri@gmail.com>
2022-02-04 15:02:54 -0300

    feat(payment):  model creetion, post route created, company by id adapted, user companny by id adapted

--- api/controllers/paymentController.ts
+++ api/controllers/paymentController.ts
@@ -7,7 +7,7 @@ mercadopago.configure({
 });
 
 module.exports = {
-  payment: async (req: Request, res: Response) => {
+  checkout: async (req: Request, res: Response) => {
     try {
       const preference = {
         items: [
@@ -28,6 +28,30 @@ module.exports = {
       res.json(response.body);
     } catch (error) {
       res.status(500).send(error);
+    }
+  },
+  payment: async (req: Request, res: Response) => {
+    try {
+      const { mercadopagoCode, date, companyId } = req.body;
+      const createdPayment = await prisma.payment.create({
+        data: {
+          mercadopagoCode,
+          date,
+          companyId: Number(companyId),
+        },
+      });
+
+      const updatedCompany = await prisma.company.update({
+        where: {
+          id: Number(companyId),
+        },
+        data: {
+          premium: true,
+        },
+      });
+      res.json(updatedCompany);
+    } catch (error) {
+      res.status(400).send(error);
     }
   },
 };
