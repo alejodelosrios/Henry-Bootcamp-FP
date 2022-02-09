0f430b0684baa66efe2efc4e3ac6aec83363ae9b
Alejandro Ramirez <ramalejandro@gmail.com>
2022-02-08 20:36:27 -0600

    styles(Create Post Form): Styles & new fields added 💄
    - Modality, contractType & salary fields adde to the form
    - Sytles added

--- client/src/components/PostCreateForm/styles.ts
+++ client/src/components/PostCreateForm/styles.ts
@@ -1,176 +1,148 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-width: 100%;
-height: 188px;
-display: flex;
-  flex-direction: column;
-  justify-content: center;
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding-bottom: 2rem;
 `
-export const ContSup=styled.div`
+export const ContSup = styled.div`
 width: 500px;
 `
 
-export const TopBackground = styled.div`
-  position: absolute;
-  height: 180px;
-  top: 0;
-  left: 0;
-  background: rgba(200, 121, 255, 0.1);
-  width: 100%;
-  z-index: 998;
-`;
-
-export const Title = styled.h1`
-font-size: 80px;
-line-height: 70px;
-text-align: center;
-align-items: center;
-top: 131px;
+export const Title = styled.h2`
+    width: 100%;
+    font-size: 1.7rem;
+    font-weight: bold;
+    color: ${(props) => props.theme.colors.typography.light};
 `
-export const PT = styled.p`
-  color: ${props => props.theme.colors.details.primary};
-  margin: 0;
-  text-align: center;
-`;
 
 export const Input = styled.input`
-width: 306px;
-  height: 42px;
-  border-radius: 15px;
-  border: 1px solid #ffb7ff;
-  background: #ffb7ff1a;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 22px;
-  letter-spacing: -0.04em;
-  text-align: left;
-  margin-top:15px;
-  margin-bottom:15px;
-  padding-left: 1rem;
-  ::placeholder {
-    color: #afb0e9;
-  }
-  :focus {
-    background: #ffb7ff30;
-    outline: none;
-  }
+    width: 100%;
+    padding: 0.7rem 1rem;
+    font-size: 1rem;
+    font-family: ${(props) => props.theme.colors.typography.openSans};
+    color: ${(props) => props.theme.colors.typography.darkest};
+    ::placeholder {
+        color: ${(props) => props.theme.colors.typography.darkest};
+    }
+    border-radius: 0.5rem;
+    border: 0.5px solid ${(props) => props.theme.colors.backgrounds.base};
+    :focus {
+        outline: none;
+    }
 `
-export const Input2 = styled.input`
-width: 180px;
-  height: 42px;
-  border-radius: 15px;
-  border: 1px solid #ffb7ff;
-  background: #ffb7ff1a;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 22px;
-  letter-spacing: -0.04em;
-  text-align: left;
-  margin-top:15px;
-  margin-bottom:15px;
-  margin-right: 12px;
-  padding-left: 1rem;
-  ::placeholder {
-    color: #afb0e9;
-  }
-  :focus {
-    background: #ffb7ff30;
-    outline: none;
-  }
+export const TagsCont = styled.div`
+    padding: 1rem 0;
+    width: 100%;
+    display: flex;
+    justify-content:flex-start;
+    align-items: center;
+    gap: 0.5rem;
 `
 
 export const Select = styled.select`
-width: 306px;
-  height: 42px;
-  border-radius: 15px;
-  border: 1px solid #ffb7ff;
-  background: #ffb7ff1a;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 22px;
-  letter-spacing: -0.04em;
-  text-align: left;
-  padding-left: 1rem;
-  margin-top:15px;
-  margin-bottom:15px;
-  ::placeholder {
-    color: #afb0e9;
-  }
-  :focus {
-    background: #ffb7ff30;
-    outline: none;
-  }
+    width: 100%;
+    padding: 0.7rem 1rem;
+    font-size: 1rem;
+    font-family: ${(props) => props.theme.colors.typography.openSans};
+    color: ${(props) => props.theme.colors.typography.darkest};
+    ::placeholder {
+        color: ${(props) => props.theme.colors.typography.darkest};
+    }
+    border-radius: 0.5rem;
+    border: 0.5px solid ${(props) => props.theme.colors.backgrounds.base};
+    :focus {
+        outline: none;
+    }
 `
 export const TextArea = styled.textarea`
-  border-radius: 15px;
-  border: 1px solid #ffb7ff;
-  background: #ffb7ff1a;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 22px;
-  letter-spacing: -0.04em;
-  text-align: left;
-  padding-left: 1rem;
-  margin-top: 20px;
-  margin-bottom: 20px;
-  ::placeholder {
-    color: #afb0e9;
-  }
-  :focus {
-    background: #ffb7ff30;
+    width: 100%;
+    padding: 0.7rem 1rem;
+    font-size: 1rem;
+    font-family: ${(props) => props.theme.colors.typography.openSans};
+    color: ${(props) => props.theme.colors.typography.darkest};
+    border-radius: 0.5rem;
+    border: 0.5px solid ${(props) => props.theme.colors.backgrounds.base};
+    ::placeholder {
+        color: ${(props) => props.theme.colors.typography.darkest};
+    }
+    :focus {
     outline: none;
-  }
+    }
 `
 
 export const Button = styled.button`
+  width:fit-content;
   justify-content: center;
   align-items: center;
-  padding: 10px 30px;
-  position: static;
-  width: 112px;
-  height: 42px;
-  left: 0px;
-  top: 250.62px;
-  background: lightblue;
+  margin-top: 1rem;
+  padding: 0.5rem 1rem;
+  background:${(props) => props.theme.colors.details.primary};
   border: none;
   color: white;
-  margin-left: 2px;
-  border-radius: 10px;
+  border-radius: 0.5rem;
   box-shadow: 4px 4px 12px 5px rgba(93, 95, 239, 0.1);
-  margin-bottom: 20px;
   cursor: pointer;
   font-family: ${(props) => props.theme.colors.typography.light};
 `;
+export const Tag = styled.button`
+  justify-content: center;
+  align-items: center;
+  padding: 0.5rem 1rem;
+  background:transparent;
+  border: 0.5px solid ${(props) => props.theme.colors.details.primary};
+  color: ${(props) => props.theme.colors.details.primary};
+  border-radius: 0.5rem;
+  box-shadow: 4px 4px 5px 5px rgba(0, 0, 0, 0.05);
+  cursor: pointer;
+  font-family: ${(props) => props.theme.colors.typography.openSans};
+`;
 
-export const Form= styled.form`
+export const Form = styled.form`
+width:100%;
 display: flex;
-justify-content:space-evenly;
-margin-top:30px;
-margin-bottom: 30px;
-
+flex-direction: column;
+justify-content:center;
+align-items:center;
 `
-export const Aside= styled.aside`
+export const Aside = styled.aside`
+width:inherit;
 display: flex;
 justify-content:space-evenly;
 flex-direction: column;
+gap: 1rem;
 `
-export const Section= styled.section`
+export const Section = styled.section`
+width:inherit;
 display: flex;
 justify-content:space-evenly;
 flex-direction:column;
 `
-export const Div= styled.div`
+export const Div = styled.div`
+width:inherit;
 display: flex;
-justify-content:space-evenly;
-flex-direction:column;
+justify-content:space-between;
+flex-wrap: wrap;
+gap:1rem;
+`
+export const InnerDiv = styled.div`
+flex: 1;
+display: flex;
+flex-direction: column;
+justify-content:flex-start;
+gap:0.3rem;
+`
+export const InnerDiv2 = styled.div`
+width:100%;
+display: flex;
+flex-direction: column;
+justify-content:flex-start;
+gap:0.3rem;
 `
 
-export const Label=styled.label`
+export const Label = styled.label`
 font-size: 18px;
 color: #757577;
 font-weight: bolder;
