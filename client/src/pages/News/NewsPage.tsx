f02a53ea778ec8706c2bdcb638608864eefb025f
Franh01 <fran_lopez9@live.com>
2022-02-09 19:46:42 -0300

    feat(news): loading added

--- client/src/pages/News/NewsPage.tsx
+++ client/src/pages/News/NewsPage.tsx
@@ -7,7 +7,9 @@ import Storage from "../../services/storage";
 export const NewsPage = () => {
     const token = Storage.get("token");
     const [news, setNews] = useState<any>([])
+    const [loading, setLoading] = useState(false);
     useEffect(()=>{
+        setLoading(true)
         axios.get(`${process.env.REACT_APP_API}/news/index`,
     {
         headers: {
@@ -16,19 +18,28 @@ export const NewsPage = () => {
     })
         .then((res)=>{
             setNews(res.data)
+            setLoading(false)
         })
     },[])
-  return (
-      <HomeLayout>
-          <div>
-          {news.length === 0 ? <div><h1 style={{textAlign: 'center'}}>Aún no hay posts disponibles...</h1></div> : <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>{news.map((news: any) => (
-              <News
-              title={news.title}
-              description={news.description}
-              image={news.image}
-              />
-          ))}</div>}
-              </div> 
-      </HomeLayout>
-  )
+  if (loading) {
+      return (
+          <HomeLayout>
+              <div><h1 style={{textAlign: 'center'}}>Cargando...</h1></div>
+          </HomeLayout>
+      )
+  } else {
+    return (
+        <HomeLayout>
+            <div>
+            {news.length === 0 ? <div><h1 style={{textAlign: 'center'}}>Aún no hay posts disponibles...</h1></div> : <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>{news.map((news: any) => (
+                <News
+                title={news.title}
+                description={news.description}
+                image={news.image}
+                />
+            ))}</div>}
+                </div> 
+        </HomeLayout>
+    )
+  }
 }
