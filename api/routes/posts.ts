e92e771b4ccb2d2bd845a8a889798fcd7a4d2f08
Julian <j.m.decurnex@gmail.com>
2022-02-10 14:03:01 -0300

    feat(routes/posts): :sparkles: added token verification

--- api/routes/posts.ts
+++ api/routes/posts.ts
@@ -1,20 +1,22 @@
 import { Router } from "express";
 const postsController = require("../controllers/postsController");
 
+const isAuthorized = require("./middlewares/isAuthorized");
+
 const postsRouter = Router();
 
 //POST
 postsRouter.post("/filter", postsController.filter);
-postsRouter.post("/create/:companyId", postsController.create);
+postsRouter.post("/create/:companyId", isAuthorized(["admin", "company"]), postsController.create);
 
 //GET
 postsRouter.get("/index", postsController.index);
 postsRouter.get("/:postId", postsController.postById);
 
 //PUT
-postsRouter.put("/update/:postId", postsController.update)
+postsRouter.put("/update/:postId", isAuthorized(["admin", "company"]), postsController.update)
 
 //DELETE
-postsRouter.delete("/delete/:postId", postsController.delete);
+postsRouter.delete("/delete/:postId", isAuthorized(["admin", "company"]), postsController.delete);
 
 module.exports = postsRouter;
