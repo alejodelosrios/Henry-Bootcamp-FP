33e5e067aba938abb57eb5ecf215bb33d198e45f
Julian <j.m.decurnex@gmail.com>
2022-02-10 14:11:03 -0300

    feat(routes/user): :sparkles: added security verification to several routes

--- api/routes/user.ts
+++ api/routes/user.ts
@@ -9,14 +9,14 @@ userRouter.post("/register", userController.register);
 userRouter.post("/login", userController.login);
 
 //GET
-userRouter.get("/index", userController.index);
+userRouter.get("/index", isAuthorized(["admin"]), userController.index);
 
 //PUT
 userRouter.put("/admin", isAuthorized(["admin"]), userController.userToAdmin)
-userRouter.put("/login/reset/:email", userController.resetPassword)
-userRouter.put("/update/:userId", userController.update);
+userRouter.put("/login/reset/:email", isAuthorized(["admin", "applicant", "company"]), userController.resetPassword)
+userRouter.put("/update/:userId", isAuthorized(["admin", "applicant", "company"]), userController.update);
 
 //DELETE
-userRouter.delete("/delete/:userId", userController.delete);
+userRouter.delete("/delete/:userId", isAuthorized(["admin", "applicant", "company"]), userController.delete);
 
 module.exports = userRouter;
