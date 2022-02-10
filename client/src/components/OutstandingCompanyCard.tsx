9413aafdcba2a8b18313cb2011d37f164cc0a193
Alejandro Ramirez <ramalejandro@gmail.com>
2022-02-10 10:40:31 -0600

    styles(Post card): User postulations cards updated 💄

--- client/src/components/OutstandingCompanyCard.tsx
+++ client/src/components/OutstandingCompanyCard.tsx
@@ -8,53 +8,54 @@ const Container = styled.div`
   align-items: center;
   justify-content: center;
   margin: 1rem 0;
-  background-color: #FFF;
-  border-radius: 1vw;
-  padding-bottom: 1vw;
-  text-align:center;
+  background-color: #fff;
+  border-radius: 1rem;
+  padding: 1rem;
+  text-align: center;
   box-shadow: -6px -2px 15px -7px rgba(0, 0, 0, 0.5);
   -webkit-box-shadow: -6px -2px 15px -7px rgba(0, 0, 0, 0.5);
   -moz-box-shadow: -6px -2px 15px -7px rgba(0, 0, 0, 0.5);
 `;
 
 const Div = styled.div`
-width: 50%;
-height: 50%;
-border-radius: 1vw 1vw 0 0 ;
-margin-bottom: 1vw;
-overflow: hidden;
-`
+  width: 50%;
+  height: 50%;
+  border-radius: 1rem 1rem 0 0;
+  margin-bottom: 1rem;
+  overflow: hidden;
+`;
 
 const Img = styled.img`
-width: 50%;
-` 
+  width: 50%;
+`;
 
 const H1 = styled.h1`
-text-transform: capitalize;
-color: rgba(239, 93, 168, 0.7);
-font-family: ${p => p.theme.colors.typography.poppins};
-font-size: 1.5rem;
-`
+  text-transform: capitalize;
+  color: rgba(239, 93, 168, 0.7);
+  font-family: ${(p) => p.theme.colors.typography.poppins};
+  font-size: 1.5rem;
+`;
 const P = styled.p`
-color: rgba(239, 93, 168, 0.5);
-text-transform: capitalize;
-
-`
+  color: rgba(239, 93, 168, 0.5);
+  text-transform: capitalize;
+`;
 type Props = {
-    id: number;
-    img: string,
-    name: string,
-    location: string
-  };
+  id: number;
+  img: string;
+  name: string;
+  location: string;
+};
 
-const OCCard: FC<Props> = ({id, img, name, location}) => {
-    return (
-      <Container>
-        <Div><Img src={img} alt={'Company Img'}/></Div>
-        <H1>{name}</H1>
-        <P>{location}</P>
-      </Container>
-    );
-  };
-  
-  export default OCCard;
+const OCCard: FC<Props> = ({ id, img, name, location }) => {
+  return (
+    <Container>
+      <Div>
+        <Img src={img} alt={"Company Img"} />
+      </Div>
+      <H1>{name}</H1>
+      <P>{location}</P>
+    </Container>
+  );
+};
+
+export default OCCard;
--- client/src/pages/MyPostulations/PostCard/styles.ts
+++ client/src/pages/MyPostulations/PostCard/styles.ts
@@ -22,69 +22,65 @@ export const ContenedorB = styled.div`
   justify-content: left;
 `;
 export const Box = styled.div`
-display: flex;
-justify-content: center;
-width: 12vw;
-height: 5vw;
-overflow: hidden;
-border-radius: 0.3vw;
-margin: 0.5vw;
-margin-bottom: 3px;
-margin-right: 1.4vw;
-vertical-align: middle;
-align-items: center;
+  display: flex;
+  justify-content: center;
+  width: 12vw;
+  height: 5vw;
+  overflow: hidden;
+  border-radius: 0.3vw;
+  margin: 0.5vw;
+  margin-bottom: 3px;
+  margin-right: 1.4vw;
+  vertical-align: middle;
+  align-items: center;
 `;
 
 export const Img = styled.img`
-object-fit: cover;
-object-position: center;
-width: 8vw;
-height: auto;
+  object-fit: cover;
+  object-position: center;
+  width: 8vw;
+  height: auto;
 `;
 
 export const ContenedorB2 = styled.div`
-display: flex;
-justify-content: space-between;
-margin-top: 10px;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 10px;
 `;
 export const ContenedorC = styled.div`
-display: flex;
-padding: 0px;
-justify-content: space-evenly;
-flex-direction: column;
-width: 458px;
-height: 76px;
-margin-right: 10px;
+  display: flex;
+  padding: 0px;
+  justify-content: space-evenly;
+  flex-direction: column;
+  width: 458px;
+  height: 76px;
+  margin-right: 10px;
 `;
 export const ContenedorF = styled.div`
-width: 100%;
-height: 25%;
-display: flex;
-justify-content: space-between;
-border-radius: 0px 0px 15px 15px;
-padding: 30px;
-padding-top: 10px;
-padding-bottom: 10px;
-background-color: #fff8ff;
+  width: 100%;
+  height: 25%;
+  display: flex;
+  justify-content: space-between;
+  border-radius: 0px 0px 15px 15px;
+  padding: 30px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  background-color: #fff8ff;
 `;
 export const Title = styled.h4`
-margin: 0px;
-font-size: 2vw;
-font-weight: 30px;
-color: #757577;
+  margin: 0px;
+  font-size: 1.7rem;
+  font-weight: 30px;
+  color: #757577;
 `;
 export const P = styled.p`
-font-size: 1.3vw;
-color: #757577;
+  font-size: 1.3rem;
+  color: #757577;
 `;
 export const P3 = styled.p`
-font-size: 1.1vw;
-color: #ef5da8;
+  font-size: 1.1rem;
+  color: #ef5da8;
 `;
 
-export const Modal = styled.div`
-  
-`
-export const Overlay = styled.div`
-
-`
+export const Modal = styled.div``;
+export const Overlay = styled.div``;
